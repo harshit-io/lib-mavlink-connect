0c5bdec27ae4de03e8ead3481173f0513d589b7d
Harshit Sharma <harshit.sharma@botlabdynamics.com>
2025-04-28 15:22:54 +0530

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,72 @@
 import { NativeModules } from 'react-native';
-
 const { LibMavlinkConnect } = NativeModules;
 
 // Export the functions from the native module
 export const telemInit = async (): Promise<string> => {
-  try {
-    const result = await LibMavlinkConnect.telemInit();
-    return result;
-  } catch (error) {
-    throw new Error("Error initializing telemetry: " + error.message);
-  }
+
+ try {
+ const result = await LibMavlinkConnect.telemInit();
+ return result;
+    }
+     catch (error) {
+      if (error instanceof Error) {
+        throw new Error("Error initializing telemetry: " + error.message);
+      } else {
+        throw new Error("Error initializing telemetry: " + String(error));
+      }
+
+ }
+
 };
+
+
 
 export const getMavlinkDataJson = async (): Promise<string> => {
-  try {
-    const result = await LibMavlinkConnect.getMavlinkDataJson();
-    return result;
-  } catch (error) {
-    throw new Error("Error fetching Mavlink data: " + error.message);
-  }
+
+ try {
+
+ const result = await LibMavlinkConnect.getMavlinkDataJson();
+
+ return result;
+
+ } catch (error) {
+
+ if (error instanceof Error) {
+
+ throw new Error("Error fetching Mavlink data: " + error.message);
+
+ } else {
+
+ throw new Error("Error fetching Mavlink data: " + String(error));
+
+ }
+
+ }
+
 };
 
+
+
 export const sendGuidedCommand = async (command: string): Promise<string> => {
-  try {
-    const result = await LibMavlinkConnect.sendGuidedCommand(command);
-    return result;
-  } catch (error) {
-    throw new Error("Error sending guided command: " + error.message);
-  }
+
+ try {
+
+ const result = await LibMavlinkConnect.sendGuidedCommand(command);
+
+ return result;
+
+ } catch (error) {
+
+ if (error instanceof Error) {
+
+ throw new Error("Error sending guided command: " + error.message);
+
+ } else {
+
+ throw new Error("Error sending guided command: " + String(error));
+
+ }
+
+ }
+
 };
